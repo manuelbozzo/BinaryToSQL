694e9159a6fd8b44ab47d095082a407f7005e5ca
manuelbozzo <36906828+manuelbozzo@users.noreply.github.com>
2018-03-03 16:26:20 -0300

    Binary Conection test
    -Added new class to connect to binary.com

--- BinaryScraper/BinaryScraper/Program.cs
+++ BinaryScraper/BinaryScraper/Program.cs
@@ -10,6 +10,22 @@ namespace BinaryScraper
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+            string data = "{\"ticks\":\"R_100\"}";
+
+            var bws = new BinaryConnection();
+            bws.Connect().Wait();
+
+            bws.SendRequest(data).Wait();
+            bws.StartListen();
+
+            Console.ReadLine();
         }
     }
 }
