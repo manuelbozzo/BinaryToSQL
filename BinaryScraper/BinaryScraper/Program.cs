c7b1b247fa065afe4c33c4cadcbfcdaccf9d0ff8
manuelbozzo <36906828+manuelbozzo@users.noreply.github.com>
2018-03-03 16:50:08 -0300

    GetSymbols WIP
    -Added GetData and GetStream methods as interfase

--- BinaryScraper/BinaryScraper/Program.cs
+++ BinaryScraper/BinaryScraper/Program.cs
@@ -15,14 +15,38 @@ namespace BinaryScraper
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+            Test();
+        }
 
+        private static void GetSymbols()
+        {
+            string request = "{ \"active_symbols\": \"brief\", \"product_type\": \"basic\"}";
+            GetData(request);
+        }
 
-            string data = "{\"ticks\":\"R_100\"}";
+        private static void Test()
+        {
+            string request = "{\"ticks\":\"R_100\"}";
+            GetStream(request);
+        }
 
+        private static void GetStream(string request)
+        {
             var bws = new BinaryConnection();
             bws.Connect().Wait();
 
-            bws.SendRequest(data).Wait();
+            bws.SendRequest(request).Wait();
+            bws.StartListen();
+
+            Console.ReadLine();
+        }
+
+        private static void GetData(string request)
+        {
+            var bws = new BinaryConnection();
+            bws.Connect().Wait();
+
+            bws.SendRequest(request).Wait();
             bws.StartListen();
 
             Console.ReadLine();
