6ce371258ffa788ee28b51a10ea58356f19bc2b4
manuelbozzo <36906828+manuelbozzo@users.noreply.github.com>
2018-03-03 21:18:13 -0300

    GetSymbols
    Working version of GetSymbols

---
+++ BinaryScraper/BinaryScraper/BinaryModels/EchoReq.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryScraper.BinaryModels
+{
+    class EchoReq
+    {
+        public string active_symbols { get; set; }
+        public string product_type { get; set; }
+    }
+}
--- BinaryScraper/BinaryScraper/Program.cs
+++ BinaryScraper/BinaryScraper/Program.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace BinaryScraper
 {
     class Program
     {
+        static List<string> symbols;
         static void Main(string[] args)
         {
-
             GetSymbols();
             Console.ReadLine();
+        }
+
+        private static List<string> ParseSymbols(string json)
+        {
+            BinaryModels.RootObject sym = JsonConvert.DeserializeObject<BinaryModels.RootObject>(json);
+
+            return sym.active_symbols.Where(x => x.market == "forex").Select(x => x.symbol).ToList();
         }
 
         private static void GetSymbols()
         {
             string request = "{ \"active_symbols\": \"brief\", \"product_type\": \"basic\"}";
-            GetData(request);
+            string json = GetData(request);
+            symbols = ParseSymbols(json);
         }
 
         private static void Test()
